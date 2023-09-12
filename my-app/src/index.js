9312d584f8462ddbdee0e96fd7e33ac6d1f444c4
Mu-jun <suo24691@gmail.com>
2023-09-13 01:36:11 +0900

    게임 완성하기
    "X"와 "O"를 번갈아 표시
    승자를 결정하는 방법

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,35 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={() => props.onClick()}
+    >
+      {props.value}
+    </button>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+/**
+ * 여러개의 자식으로부터 데이터를 모으거나
+ * 두 개의 자식 컴포넌트들이 서로 통신하게 하려면
+ * 부모 컴포넌트에 공유 state를 정의해야 합니다.
+ * 부모 컴포넌트는 props를 사용하여 자식 컴포넌트에 state를 다시 전달할 수 있습니다.
+ * 이것은 자식 컴포넌트들이 서로 또는 부모 컴포넌트와 동기화 하도록 만듭니다.
+ */
+class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
-  render() {
+
+  handleClick(i) {
+    const squares = this.state.squares.slice(); // 불변성을 위한 사본만들기
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
     return (
-      <button
-        className="square"
-        onClick={() => {
-          console.log('click');
-          this.setState({ value: 'X' })
-        }}
-      >
-        {this.state.value}
-      </button>
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
     );
   }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
