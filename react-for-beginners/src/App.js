310e9b77b7d9657f7b7a07d390fc97232ce9719e
Mu-jun <suo24691@gmail.com>
2023-09-19 15:53:41 +0900

    prop type 체크 & css  적용하기

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,9 +1,13 @@
+import Button from './Button';
+import styles from './App.module.css';
+
 function App() {
   return (
     <div>
-      <h1>
+      <h1 className={styles.title}>
         Hello React!!
       </h1>
+      <Button text={"Nice Button"} />
     </div>
   );
 }
