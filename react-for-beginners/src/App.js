1f195578a405f898941d128e4f2dd611db129f53
Mu-jun <suo24691@gmail.com>
2023-09-20 15:29:51 +0900

    cleanUp
    index.js에서 StrictMode 사용 시 console.log가 double invoke가 발생함

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,36 +1,25 @@
 import styles from './App.module.css';
 import { useEffect, useState } from 'react';
 
+function Hello() {
+  const cleanUp = () => {
+    console.log("Destoryed");
+  }
+  useEffect(() => {
+    console.log("Created");
+    return cleanUp;
+  }, [])
+  return <h1 className={styles.title}>Hello!!</h1>;
+}
+
 function App() {
-  const [value, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
+  const [hideFlag, setHideFlag] = useState(false);
+  const onClick = () => setHideFlag(prev => !prev);
 
-  const onClick = () => setValue(prev => prev + 1);
-
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log('This run all the time');
-
-  useEffect(() => {
-    console.log('This run once');
-  }, []);
-  useEffect(() => {
-    console.log('Search for ', keyword);
-  }, [keyword])
-  useEffect(() => {
-    console.log('count: ', value);
-  }, [value]);
-  useEffect(() => {
-    console.log('change keyword or counter')
-  }, [keyword, value]);
-  
   return (
     <div>
-      <input onChange={onChange}></input>
-      <h1 className={styles.title}>
-        {value}
-      </h1>
-      <button onClick={onClick}>Value up</button>
+      <button onClick={onClick}>{hideFlag ? "show" : "hide"}</button>
+      {hideFlag ? null : <Hello />}
     </div>
   );
 }
