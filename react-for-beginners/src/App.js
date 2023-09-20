ccc9b6e2014d9ebab422d3402eada1c5f56cf5e8
Mu-jun <suo24691@gmail.com>
2023-09-20 14:56:29 +0900

    Deps

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,18 +3,30 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [value, setValue] = useState(0);
-  const onClick = () => {
-    setValue(prev => prev + 1);
-  };
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setValue(prev => prev + 1);
+
+  const onChange = (event) => setKeyword(event.target.value);
 
   console.log('This run all the time');
 
   useEffect(() => {
     console.log('This run once');
   }, []);
-
+  useEffect(() => {
+    console.log('Search for ', keyword);
+  }, [keyword])
+  useEffect(() => {
+    console.log('count: ', value);
+  }, [value]);
+  useEffect(() => {
+    console.log('change keyword or counter')
+  }, [keyword, value]);
+  
   return (
     <div>
+      <input onChange={onChange}></input>
       <h1 className={styles.title}>
         {value}
       </h1>
