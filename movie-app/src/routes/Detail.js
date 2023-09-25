c452c0e904ed8013475f8f807e1662a7afa7980a
Mu-jun <suo24691@gmail.com>
2023-09-25 14:52:19 +0900

    github.io 배포

--- movie-app/src/routes/Detail.js
+++ movie-app/src/routes/Detail.js
@@ -2,17 +2,18 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 function Detail() {
   const [movie, setMovie] = useState({});
-
   const { id } = useParams();
-  const getMovie = async () => {
+
+  const getMovie = async (id) => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     setMovie(json.data.movie);
   };
+
   useEffect(() => {
-    getMovie();
-  }, []);
+    getMovie(id);
+  }, [id]);
   console.log(movie);
   return <h1>Detail - {id}</h1>;
 }
